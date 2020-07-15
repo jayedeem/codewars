c7ca6cb398787cb06e8ad921f6e5b97d566f2db9
Andrew Cacayuran <supreme@Drews-MacBook-Pro.local>
2020-07-15 11:57:23 -0700

    complete

---
+++ 8KYU/completed/thinkful-logic-drills-traffic-light/index.js
@@ -0,0 +1,18 @@
+/**
+ * You're writing code to control your town's traffic lights. You need a function to handle each change from green, to yellow, to red, and then to green again.
+
+Complete the function that takes a string as an argument representing the current state of the light and returns a string representing the state the light should change to.
+
+For example, update_light('green') should return 'yellow'.
+ */
+
+const config = {
+  green: 'yellow',
+  yellow: 'red',
+  red: 'green',
+};
+
+function updateLight(current) {
+  return config[current];
+}
+console.log(updateLight('red'));
